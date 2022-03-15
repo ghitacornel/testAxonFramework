d5bc0e3540e8e89b1bdbcfc7e447201cdf37511a
cornel <ghita.cornel@gmail.com>
2022-03-15 09:59:07 +0200

    better

--- src/main/java/beans/controllers/OrderRestEndpoint.java
+++ src/main/java/beans/controllers/OrderRestEndpoint.java
@@ -17,6 +17,7 @@ import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
+// facade for all endpoints
 @RestController
 @RequiredArgsConstructor
 public class OrderRestEndpoint {
